c540e33937efd0e522a48335ec3ed7823a4dc82f
rapaka.chandrashekar@gmail.com <rapaka.chandrashekar@gmsil.com>
2022-07-05 01:15:36 +0530

    done

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   }
   };
 
+  export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '440a1ef350msh58ecb762a59e282p186cdfjsn2a275e379329',
+  },
+};
+
 export const fetchData= async(url,options)=>{
     const response =await fetch(url,options);
     const data= await response.json();
